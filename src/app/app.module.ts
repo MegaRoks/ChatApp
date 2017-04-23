e8188e9e0f221e4822e7f4366fe6a766254a31a5
Maksim <MegaRoks@gmail.com>
2017-04-23 22:27:13 +0600

    I have created a chat using Angular 2 + Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDmy5JXaDolqRFpHatqsfgXOhw_34Irk5w",
+    authDomain: "mychatapp-fc11d.firebaseapp.com",
+    databaseURL: "https://mychatapp-fc11d.firebaseio.com",
+    projectId: "mychatapp-fc11d",
+    storageBucket: "mychatapp-fc11d.appspot.com",
+    messagingSenderId: "114595676557"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
